0d5b0a71d805d45c4e0316a83d34eadd0afca670
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-02-27 11:13:36 +0600

    Add basic data types and array implementation in module1

---
+++ module1/src/1.4.ts
@@ -0,0 +1,22 @@
+// Basic data type
+// string
+let firstName= 'siam';
+let roll:number = 123
+// boolean
+let isAdmin: boolean = true;
+// undefined 
+let x: undefined = undefined;
+// null
+let y: null = null; 
+
+let d: number;
+d= 10
+
+// Array
+let friends: string[] = ['siam', 'sakib', 'sajid', 'sakib'];
+let eligibleRollList: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+eligibleRollList.push();
+
+// Tuple --> array --> order --> types of values
+
+let student: [string, number, boolean] = ['siam', 123, true];
